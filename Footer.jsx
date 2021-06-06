0b32288084f3e6b2e0ae36a1ad120fd71a0e5b8c
Soniayadav135 <85388776+Soniayadav135@users.noreply.github.com>
2021-06-05 18:51:45 -1200

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+  <footer>
+    <p>Copyright @ 2021</p>
+ </footer>
+  );
+}
+
+export default Footer;
+
---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return  (
+  <div className="note">
+    <h2>Javascipt and React.js</h2>
+    <p>
+      This was an amazing bootcamp taken by Shaurya Sinha 
+      we covered everything from Scratch including Javascipt
+      React.js, HTML.
+      </p>
+  </div>
+  );
+}
+
+export default Note;
